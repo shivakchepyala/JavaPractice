6398b5bb438cca496cb2a52f6a5b3cae86cd470e
shivakchepyala <shivakchepyala@gmail.com>
2020-10-04 00:33:51 +0530

    Sample Java Practice

---
+++ src/Account.java
@@ -0,0 +1,45 @@
+
+public class Account 
+{
+	int a;
+	int b;
+	
+	public void setData(int a, int b)
+	{
+		this.a = a;
+		System.out.println("Current Value of A is: "+this.a);
+		this.b = b;
+	}
+	
+	public void showData()
+	{
+		System.out.println("Value of A is: "+a);
+		System.out.println("Value of B is: "+b+"\n");
+	}
+	
+	public static void main(String[] args) {
+		Account obj = new Account();
+		obj.setData(3, 6);
+		obj.showData();
+		
+		
+		
+		try {
+			int x = 30;
+			int y = 0;
+			int z = x/y;
+			System.out.println("Result of z is: "+z);
+		}
+		/*catch(ArithmeticException e)
+		{
+			System.out.println("Number cannot be divided by zero");
+		}*/
+		finally {
+			System.out.println("\n");
+			System.out.println("Executed");
+		}
+		
+		
+	}
+
+}
--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 
-public class test {
+public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
