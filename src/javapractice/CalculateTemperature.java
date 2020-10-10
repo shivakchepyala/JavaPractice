4bb1b2a3af9b603f11b0850502af7a2cb1eeb76b
shivakchepyala <shivakchepyala@gmail.com>
2020-10-11 00:16:37 +0530

    Update

---
+++ src/javapractice/CalculateTemperature.java
@@ -0,0 +1,16 @@
+package javapractice;
+
+//This program is written to convert the temperature
+public class CalculateTemperature {
+
+	public static void main(String[] args) {
+	 float tempInCentigrade;
+	 int tempInFahrenheit=10;
+	 tempInCentigrade = (tempInFahrenheit - 32) * 5/9;
+	 System.out.println("Temperature in Centigrade: "+tempInCentigrade);
+	 
+	 
+
+	}
+
+}
